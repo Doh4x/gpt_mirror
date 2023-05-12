344e22c6eb9aabc2acba4c7727a7110fc55e8806
Glitch (violet-smooth-molybdenum) <none>
2023-05-12 03:43:53 +0000

    🚩🚦 Checkpoint ./install.sh:210195/399

---
+++ install.sh
@@ -0,0 +1,7 @@
+set -eux
+# Two things:
+# 1. Python doesn't come with pip. Setting up a venv installs pip.
+# 2. This keeps us from installing dependencies into /app, which saves 
+python3 -m venv /tmp/venv
+. /tmp/venv/bin/activate
+python3 -m pip install -r requirements-stealth.txt
