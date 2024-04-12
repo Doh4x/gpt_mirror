944370b132d4dde17ac44aa85688e74cdb882271
Glitch (gpt-mirror-api) <none>
2024-04-12 02:59:57 +0000

    🍘🏐 Checkpoint ./server.py:32663778/182

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def home():
       client = Client()
       
       response = client.chat.completions.create(
-          model="gpt-3.5-turbo-16k",
+          model="gpt-3.5-turbo",
           messages=[{"role": "user", "content": txt}],
       )
       
