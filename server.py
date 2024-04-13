365ca9c5de094542e146078b906b76d94efc0cf5
Glitch (gpt-mirror-api) <none>
2024-04-13 10:05:25 +0000

    🍡🎙 Checkpoint ./server.py:32663778/1034

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from flask import Flask, request, jsonify
 from g4f.client import Client
@@ -8,27 +7,26 @@ app = Flask(__name__)
 
 client = Client()
 
-print('This is error output', file=sys.stderr)
-print('This is standard output', file=sys.stdout)
-
 @app.route('/', methods=['GET'])
 
 def home():
-  print("GOT REQUEST")
-  
   if "txt" in request.args:
     txt = request.args['txt']
     response = ""
   
-    try:   
-      response = client.chat.completions.create(
+    print(txt, flush=True)
+    
+    #try:
+    response = client.chat.completions.create(
           model='pi',
           messages=[{"role": "user", "content": txt}],
-      )
+    )
       
-      response = response
-    finally:
-      return jsonify({"status": "OK", "text": response})
+    print("request.args", flush=True)
+        
+    response = response.choices[0].message.content
+   # finally:
+    return jsonify({"status": "OK", "text": response})
   else:
     return jsonify({"status": "OK", "text": ""})
 
