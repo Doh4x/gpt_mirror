418d63776d3deab3b3bd005df92963d2cb83c280
Glitch (gpt-mirror-api) <none>
2024-04-14 11:36:35 +0000

    🌷🌥 Checkpoint ./server.py:32663778/5903

--- server.py
+++ server.py
@@ -15,44 +15,21 @@ app = Flask(__name__)
 
 client = Client()
 
-ai_models = ['gpt-3.5-long', 'gpt-3.5-turbo', 'Llama-2-70b-chat-hf', 'dolphin-mixtral-8x7b']
+ai_models = ['gpt-3.5-long', 'gpt-3.5-turbo', 'llama2-70b', 'dolphin-mixtral-8x7b']
 
 gpt35_error_messages = [
   '\u6d41\u91cf\u5f02\u5e38,\u8bf7\u5c1d\u8bd5\u66f4\u6362\u7f51\u7edc\u73af\u5883', 
   '\u5f53\u524d\u5730\u533a\u5f53\u65e5\u989d\u5ea6\u5df2\u6d88\u8017\u5b8c, \u8bf7\u5c1d\u8bd5\u66f4\u6362\u7f51\u7edc\u73af\u5883'
-                       ]
-def TryRequest(provider, txt):
-  response = ""
-  valid_provider = ""
-  
-  try:
-        client = Client()
-        
-        response = client.chat.completions.create(
-            model=provider,
-            messages=[{"role": "user", "content": txt}],
-        )
-        
-        print(response.choices[0].message.content, flush=True)
-        
-        if response.choices[0].message.content in gpt35_error_messages:
-          return response, valid_provider
-            
-        response = response.choices[0].message.content
-        valid_provider = provider
-  finally:
-        return response, valid_provider
-      
-@app.route('/', methods=['GET'])
+]
 
-def home():
+@app.route('/try_models', methods=['GET'])
+
+def try_models():
   if "txt" in request.args:
     txt = request.args['txt']
     response = ""
     valid_provider = ""
     
-    #while response == "":
-      #response, valid_provider = TryRequest("gpt-3.5-long", txt)
     for provider in ai_models:
       print(provider, flush=True)
       
@@ -83,6 +60,48 @@ def home():
   else:
     return jsonify({"status": "OK", "text": ""})
 
+@app.route('/try_single_model', methods=['GET'])
+
+def single_model_request():
+  if "txt" and "model" in request.args:
+    txt = request.args['txt']
+    model = request.args['model']
+    
+    print(txt,flush=True)
+    print(model,flush=True)
+    
+    response = ""
+    valid_provider = ""
+    
+    while response == "":
+       response = ""
+       valid_provider = ""
+  
+       try:
+        client = Client()
+        
+        response = client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": txt}],
+        )
+        
+        print(response.choices[0].message.content, flush=True)
+        
+        if response.choices[0].message.content in gpt35_error_messages:
+          return response, valid_provider
+            
+        response = response.choices[0].message.content
+        valid_provider = provider
+       finally:
+        return response, valid_provider
+
+    if response == "":
+       return jsonify({"status": "NOT OK", "text": "Invalid cookies", "provider": ""})
+    
+    return jsonify({"status": "OK", "text": response or "", "provider": valid_provider})
+  else:
+    return jsonify({"status": "OK", "text": ""})
+  
 @app.route('/', methods=['HEAD'])
 
 def head():
