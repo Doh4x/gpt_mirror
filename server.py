b15ccf6203a092e3d5cbe574ad5c284b39eb2dd4
Glitch (gpt-mirror-api) <none>
2024-04-14 11:00:56 +0000

    🔨👬 Checkpoint ./requirements-stealth.txt:32663778/70 ./server.py:32663778/91

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 client = Client()
 
-ai_models = ['gpt-3.5-turbo', 'llama2-70b', 'dolphin-mixtral-8x7b']
+ai_models = ['gpt-3.5-long', 'gpt-3.5-turbo', 'CodeLlama-70b-Instruct-hf', 'dolphin-mixtral-8x7b']
 
 gpt35_error_messages = [
   '\u6d41\u91cf\u5f02\u5e38,\u8bf7\u5c1d\u8bd5\u66f4\u6362\u7f51\u7edc\u73af\u5883', 
